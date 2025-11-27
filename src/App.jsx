0eb8187d82cd96a68583e5fe1a752955faaa2b18
amirNiazi <amir.f.web@example.com>
2025-11-27 07:41:03 -0800

    state-325

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { Fragment } from "react"
-import Car from "./Car"
+import { useState } from "react"
 
 function App() {
   
-  let userName = prompt("enter the name :: ")
-  
+  const [txt, setTxt] = useState("Hello")
+
+  const changeTxt = () => {
+    setTxt("Hey, my name's ali")
+  }
+
   return (
     <div>
     
-      <h1>WELCOM</h1>
-      {userName === "amir" ? <h3>hello amir :)</h3> : <h3>please com Back {userName} :( </h3>}
-    
-      <Car  make="Honda" model="2024" year="2001" color="red" price="1_000_400" />
-      <Car  make="BMW" model="2030" year="2024" color="blue" price="3_600_000" />
-    
+      <button onClick={changeTxt}>click me for edit text</button>
+      <p>{txt}</p>
+
     </div>
   )
 }
