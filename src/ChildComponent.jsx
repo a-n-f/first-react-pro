81e72107a9c4f92b62f796d1cd8af8d935b819bd
amir mohammad <amir.f.web@example.com>
2025-11-30 07:41:37 -0800

    EventHandling-330

--- src/ChildComponent.jsx
+++ src/ChildComponent.jsx
@@ -1,11 +1,21 @@
 const ChildComponent = ({name , age , job}) => {
     
+    const handelDelet = (e) => {
+        console.log("DELET" , e);
+    }
+
+    const handelChange = (e, txt) => {
+        console.log(e.target.value , txt);
+    }
+
     return (
         <div>
             <ul>
                 <li><strong>name:</strong> {name}</li>
                 <li><strong>age:</strong> {age}</li>
                 <li><strong>job:</strong> {job}</li>
+                <li><button onClick={handelDelet}>Delet</button></li>
+                <li><input onChange={(e) => handelChange(e, "test")} /></li>
             </ul>
         </div>
     )
