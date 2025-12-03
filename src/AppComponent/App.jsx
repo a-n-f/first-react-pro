a6cd635de20627d10dc870205950fb460e5d7002
amir mohammad <amir.f.web@example.com>
2025-12-03 07:50:20 -0800

    TailwindInReact-342

---
+++ src/AppComponent/App.jsx
@@ -0,0 +1,17 @@
+// import style from "./App.module.css";
+
+
+function App() {
+
+  console.log("App Fn");
+  
+  return (
+    <div className="">
+    
+      <h1 className="bg-red-400">my name's amir</h1>
+      <button>click me</button>
+    </div>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './main.css'
-import App from './App.jsx'
+import App from './AppComponent/App.jsx'
 
 createRoot(document.getElementById('root')).render(
     <App />
